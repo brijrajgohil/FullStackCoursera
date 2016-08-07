2cb633f0abf2b61f239c49258ce690ac8c3663c2
Brijrajsinh Gohil <brijrajsinhgohil@yahoo.com>
2016-08-07 18:47:09 +0530

    Advanced directives

--- Practice/app.js
+++ Practice/app.js
@@ -1,7 +1,30 @@
-angular.module('greetings', [])
-.directive('welcome', function () {
-    return {
-        restrict: 'E',
-        template: "<div><h1>Welcome</h1></div>"
+function FunCtrl() {
+  var self = this;
+
+  self.start = function() {
+    console.log("Fun times have been started!");
+  }
+
+  self.end = function() {
+      console.log("OVER!");
+  }
+
+};
+
+
+angular.module('coolApp', [])
+.controller('FunCtrl', FunCtrl)
+.directive("entering", function(){
+ return function(scope, element, attrs) {
+      element.bind("mouseenter", function(){
+        scope.$apply(attrs.entering);
+      })
+    }
+})
+.directive("leaving", function() {
+    return function(scope, element, attrs) {
+        element.bind("mouseleave", function() {
+            scope.$apply(attrs.leaving);
+        })
     }
 });
