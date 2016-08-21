6206b2fb5a66edb24578fa08712fe6ca5357027c
Brijrajsinh Gohil <brijrajsinhgohil@yahoo.com>
2016-08-21 13:02:47 +0530

    Prac

---
+++ Practice/invoice3.js
@@ -0,0 +1,15 @@
+angular.module('invoice3', ['finance3'])
+.controller('InvoiceController', ['currencyConvertor', function InvoiceController(currencyConvertor) {
+    this.qty = 1;
+    this.cost = 2;
+    this.inCurr = 'EUR';
+    this.currencies = currencyConvertor.currencies;
+
+    this.total = function total(outCurr) {
+        return currencyConvertor.convert(this.qty * this.cost, this.inCurr, outCurr);
+    };
+
+    this.pay = function pay() {
+        window.alert('Thanks!');
+    };
+}]);
