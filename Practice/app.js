13ee27700aab054bbcc16951ee1c7098bc5c730b
Brijrajsinh Gohil <brijrajsinhgohil@yahoo.com>
2016-08-29 11:47:46 +0530

    Had a break!

--- Practice/app.js
+++ Practice/app.js
@@ -1,7 +1,23 @@
-angular.module('eventExample', [])
-.controller('EventController', ['$scope', function($scope) {
-    $scope.count = 0;
-    $scope.$on('MyEvent', function() {
-        $scope.count++;
-    });
-}]);
+angular.module('docsIsoFnBindExample', [])
+.controller('Controller', ['$scope', function($scope) {
+    $scope.name = 'Tobias';
+    $scope.message = '';
+    $scope.hideDialog = function() {
+        $scope.message = message;
+        $scope.dialogIsHidden = true;
+        $timeout(function () {
+            $scope.message = '';
+            $scope.dialogIsHidden = false;
+        }, 2000);
+    }
+}])
+.directive('myDialog', function() {
+    return {
+        restrict: 'E',
+        transclude: true,
+        scope: {
+            'close': '&onClose'
+        },
+        templateUrl: 'my-customer-iso.html'
+    };
+});
