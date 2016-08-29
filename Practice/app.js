86da1bb361b07fa6f3beff12044d7eca413b0a74
Brijrajsinh Gohil <brijrajsinhgohil@yahoo.com>
2016-08-29 16:24:29 +0530

    Completed directives

--- Practice/app.js
+++ Practice/app.js
@@ -1,23 +1,39 @@
-angular.module('docsIsoFnBindExample', [])
-.controller('Controller', ['$scope', function($scope) {
-    $scope.name = 'Tobias';
-    $scope.message = '';
-    $scope.hideDialog = function() {
-        $scope.message = message;
-        $scope.dialogIsHidden = true;
-        $timeout(function () {
-            $scope.message = '';
-            $scope.dialogIsHidden = false;
-        }, 2000);
-    }
-}])
-.directive('myDialog', function() {
+angular.module('docsTabsExample', [])
+.directive('myTabs', function() {
     return {
         restrict: 'E',
-        transclude: true,
-        scope: {
-            'close': '&onClose'
-        },
-        templateUrl: 'my-customer-iso.html'
+        transclue: true,
+        scope: {},
+        controller: ['$scope', function MyTabsController($scope) {
+            var panes = $scope.panes = [];
+            $scope.select = function(pane) {
+                angular.forEach(panes, function(pane) {
+                    pane.selected = false;
+                });
+                pane.selected = true;
+            };
+
+            this.addPane = function(pane) {
+                if(panes.length === 0) {
+                    $scope.select(pane);
+                }
+                panes.push(pane);
+            };
+        }],
+        templateUrl: 'my-tabs.html'
+    };
+})
+.directive('myPane', function() {
+    return {
+         require: '^^myTabs',
+         restrict: 'E',
+         transclude: true,
+         scope: {
+             title: '@'
+         },
+         link: function(scope, element, attrs, tabsCtrl) {
+             tabsCtrl.addPane(scope);
+         },
+         templateUrl: 'my-pane.html'
     };
 });
