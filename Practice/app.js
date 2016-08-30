ff58f6d78846b2e755e3c562f2a48bd572708392
Brijrajsinh Gohil <brijrajsinhgohil@yahoo.com>
2016-08-30 16:30:30 +0530

    Egghead AngularJS

--- Practice/app.js
+++ Practice/app.js
@@ -1,1 +1,17 @@
-angular.module('heroApp', []);
+angular.module('myApp', [])
+.factory('Data', function() {
+    return {
+        message: "Hello"
+    }
+})
+.filter('reverse', function() {
+    return function(text) {
+        return text.split("").reverse().join("");
+    }
+})
+.controller('FirstCtrl', ['$scope', 'Data', function($scope, Data) {
+    $scope.data = Data;
+}])
+.controller('SecondCtrl', ['$scope', 'Data', function($scope, Data) {
+    $scope.data = Data;
+}]);
