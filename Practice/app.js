ae61654b78d49d590b51173e8d788f5c97e54491
Brijrajsinh Gohil <brijrajsinhgohil@yahoo.com>
2016-08-31 17:48:42 +0530

    Directive to Directive Communication

--- Practice/app.js
+++ Practice/app.js
@@ -1,17 +1,56 @@
-angular.module('myApp', [])
-.factory('Data', function() {
+var app = angular.module('superApp', []);
+
+app.directive("superhero", [function() {
     return {
-        message: "Hello"
-    }
-})
-.filter('reverse', function() {
-    return function(text) {
-        return text.split("").reverse().join("");
-    }
-})
-.controller('FirstCtrl', ['$scope', 'Data', function($scope, Data) {
-    $scope.data = Data;
-}])
-.controller('SecondCtrl', ['$scope', 'Data', function($scope, Data) {
-    $scope.data = Data;
+        restrict: 'E',
+
+        controller: ['$scope', function($scope) {
+            $scope.abilities = [];
+
+            this.addStrength = function() {
+                $scope.abilities.push('strength');
+            };
+
+            this.addSpeed = function() {
+                $scope.abilities.push('speed');
+            };
+
+            this.addFlight = function() {
+                $scope.abilities.push('flight');
+            };
+        }],
+
+        link: function(scope, element, attrs) {
+            element.bind('mouseenter', function() {
+                console.log(scope.abilities);
+            })
+        }
+    };
+}]);
+
+app.directive('strength', [function() {
+    return {
+        require: 'superhero',
+        link: function(scope, element, attrs, supreheroCtrl) {
+            supreheroCtrl.addStrength();
+        }
+    };
+}]);
+
+app.directive('speed', [function() {
+    return {
+        require: 'superhero',
+        link: function(scope, element, attrs, supreheroCtrl) {
+            supreheroCtrl.addSpeed();
+        }
+    };
+}]);
+
+app.directive('flight', [function() {
+    return {
+        require: 'superhero',
+        link: function(scope, element, attrs, supreheroCtrl) {
+            supreheroCtrl.addFlight();
+        }
+    };
 }]);
