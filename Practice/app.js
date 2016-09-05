72ff651f277569e24819578b48dbfb302d191e7e
Brijrajsinh Gohil <brijrajsinhgohil@yahoo.com>
2016-09-05 10:21:01 +0530

    Resolve, Event Changes

--- Practice/app.js
+++ Practice/app.js
@@ -1,56 +1,45 @@
-var app = angular.module('superApp', []);
+var app = angular.module('app', ['ngRoute']);
 
-app.directive("superhero", [function() {
-    return {
-        restrict: 'E',
-
-        controller: ['$scope', function($scope) {
-            $scope.abilities = [];
-
-            this.addStrength = function() {
-                $scope.abilities.push('strength');
-            };
-
-            this.addSpeed = function() {
-                $scope.abilities.push('speed');
-            };
-
-            this.addFlight = function() {
-                $scope.abilities.push('flight');
-            };
-        }],
-
-        link: function(scope, element, attrs) {
-            element.bind('mouseenter', function() {
-                console.log(scope.abilities);
-            })
+app.config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+    .when('/', {
+        templateUrl: 'app.html',
+        controller: 'ViewCtrl',
+    })
+    .when('/new', {
+        templateUrl: 'new.html',
+        controller: 'NewCtrl',
+        resolve: {
+            loadData: viewCtrl.loadData
         }
-    };
+    })
 }]);
 
-app.directive('strength', [function() {
-    return {
-        require: 'superhero',
-        link: function(scope, element, attrs, supreheroCtrl) {
-            supreheroCtrl.addStrength();
-        }
-    };
+app.controller('AppCtrl', ['$scope', '$rootScope', '$route', '$location', function($scope, $rootScope, $route, $location) {
+    $rootScope.$on("$routeChangeStart", function(event, current, previous) {
+        console.log($scope, $rootScope, $route, $location);
+    })
+
+    $rootScope.$on("$routeChangeSuccess", function(event, current, previous) {
+        console.log($scope, $rootScope, $route, $location);
+    })
 }]);
 
-app.directive('speed', [function() {
-    return {
-        require: 'superhero',
-        link: function(scope, element, attrs, supreheroCtrl) {
-            supreheroCtrl.addSpeed();
-        }
-    };
+var viewCtrl = app.controller('ViewCtrl', ['$scope', '$routeParams', '$location', function($scope, $routeParams, $location) {
+    $scope.changeRoute = function() {
+        console.log($scope);
+        $location.path('/new');
+    }
 }]);
 
-app.directive('flight', [function() {
-    return {
-        require: 'superhero',
-        link: function(scope, element, attrs, supreheroCtrl) {
-            supreheroCtrl.addFlight();
-        }
-    };
-}]);
+app.controller('NewCtrl', ['$scope', 'loadData', '$template', function($scope, $loadData, $template) {
+    console.log($scope, loadData, $template);
+}])
+
+viewCtrl.loadData = function($q, $timeout) {
+    var defer = $q.defer();
+    $timeout(function() {
+        defer.resolve({message: "Success!"});
+    }, 1000);
+    return defer.promise;
+};
