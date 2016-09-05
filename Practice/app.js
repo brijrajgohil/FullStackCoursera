eb873b01518e1b2405db667fd5967824e1b935f9
Brijrajsinh Gohil <brijrajsinhgohil@yahoo.com>
2016-09-05 14:59:22 +0530

    9 More videos to go

--- Practice/app.js
+++ Practice/app.js
@@ -1,34 +1,6 @@
 var app = angular.module('app', ['ngAnimate']);
 
-app.directive("country", [function() {
-    return {
-        restrict: "E",
-        controller: function() {
-            this.makeAnnouncement = function(message) {
-                console.log("Contry says " + message);
-            }
-        }
-    }
-}]);
 
-app.directive("state", [function() {
-    return {
-        restrict: "E",
-        controller: function() {
-            this.makeLaw = function(law) {
-                console.log("Law: " + law);
-            }
-        }
-    }
-}]);
-
-app.directive("city", [function() {
-    return {
-        restrict: "E",
-        require: ['^country', '^state'],
-        link: function(scope, element, attrs, ctrls) {
-            ctrls[0].makeAnnouncement("from city");
-            ctrls[1].makeLaw("Jumg Higher");
-        }
-    }
+app.controller('AppCtrl', [function() {
+    
 }])
