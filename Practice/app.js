462c6e15f50b10dfbefc5cf2e9fc20a388735ee9
Brijrajsinh Gohil <brijrajsinhgohil@yahoo.com>
2016-09-06 17:26:14 +0530

    Back to Thinskster

--- Practice/app.js
+++ Practice/app.js
@@ -1,6 +1,17 @@
 var app = angular.module('app', ['ngAnimate']);
 
 
-app.controller('AppCtrl', [function() {
-    
-}])
+app.directive('dumbPassword', [function() {
+    return {
+        restrict: 'E',
+        replace: true,
+        templateUrl: 'dumbpass.html',
+        link: function(scope, element) {
+            scope.$watch('model.input', function(value) {
+                if(value === 'password') {
+                    element.children(1).toggleClass('alert-box alert');
+                }
+            });
+        }
+    };
+}]);
