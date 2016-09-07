c7e2d0e0688f111a4fb8cd94bbee062637eb4b7c
Brijrajsinh Gohil <brijrajsinhgohil@yahoo.com>
2016-09-07 16:00:53 +0530

    Animations in Angular

--- Practice/app.js
+++ Practice/app.js
@@ -1,17 +1,6 @@
 var app = angular.module('app', ['ngAnimate']);
 
-
-app.directive('dumbPassword', [function() {
-    return {
-        restrict: 'E',
-        replace: true,
-        templateUrl: 'dumbpass.html',
-        link: function(scope, element) {
-            scope.$watch('model.input', function(value) {
-                if(value === 'password') {
-                    element.children(1).toggleClass('alert-box alert');
-                }
-            });
-        }
-    };
-}]);
+app.controller('AppCtrl', [function() {
+    this.lightTheme = false;
+    this.bigger = false;
+}])
