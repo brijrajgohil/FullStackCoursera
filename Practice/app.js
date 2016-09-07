38ef75396d4757c6d691a157d4ea74df162d6fc1
Brijrajsinh Gohil <brijrajsinhgohil@yahoo.com>
2016-09-07 16:54:00 +0530

    Templating, now $http

--- Practice/app.js
+++ Practice/app.js
@@ -1,6 +1,19 @@
-var app = angular.module('app', ['ngAnimate']);
+(function(){
 
-app.controller('AppCtrl', [function() {
-    this.lightTheme = false;
-    this.bigger = false;
-}])
+  function TestCtrl($templateCache) {
+    this.user = {name: 'Blake'};
+
+    console.log($templateCache.get('test.html'));
+  }
+
+  angular.module('app', ['ngRoute'])
+  .config(function($routeProvider){
+    $routeProvider.when('/', {
+      controller: 'TestCtrl as test',
+       templateUrl: 'test.html'
+    })
+    .otherwise('/');
+  })
+  .controller('TestCtrl', TestCtrl);
+
+})()
