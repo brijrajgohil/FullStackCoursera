38fefc53a208c886b2f3150ba7471ea2c6482c2c
Brijrajsinh Gohil <brijrajsinhgohil@yahoo.com>
2016-09-11 12:09:43 +0530

    Authenticating using JWT

--- Practice/app.js
+++ Practice/app.js
@@ -1,19 +1,71 @@
-(function(){
+;(function(){
+function authInterceptor(API, auth) {
+  return {
+    // automatically attach Authorization header
+    request: function(config) {
+      return config;
+    },
 
-  function TestCtrl($templateCache) {
-    this.user = {name: 'Blake'};
+    // If a token was sent back, save it
+    response: function(res) {
+      return res;
+    },
+  }
+}
 
-    console.log($templateCache.get('test.html'));
+function authService($window) {
+  var self = this;
+
+  // Add JWT methods here
+}
+
+function userService($http, API, auth) {
+  var self = this;
+  self.getQuote = function() {
+    return $http.get(API + '/auth/quote')
   }
 
-  angular.module('app', ['ngRoute'])
-  .config(function($routeProvider){
-    $routeProvider.when('/', {
-      controller: 'TestCtrl as test',
-       templateUrl: 'test.html'
-    })
-    .otherwise('/');
-  })
-  .controller('TestCtrl', TestCtrl);
+  // add authentication methods here
 
-})()
+}
+
+// We won't touch anything in here
+function MainCtrl(user, auth) {
+  var self = this;
+
+  function handleRequest(res) {
+    var token = res.data ? res.data.token : null;
+    if(token) { console.log('JWT:', token); }
+    self.message = res.data.message;
+  }
+
+  self.login = function() {
+    user.login(self.username, self.password)
+      .then(handleRequest, handleRequest)
+  }
+  self.register = function() {
+    user.register(self.username, self.password)
+      .then(handleRequest, handleRequest)
+  }
+  self.getQuote = function() {
+    user.getQuote()
+      .then(handleRequest, handleRequest)
+  }
+  self.logout = function() {
+    auth.logout && auth.logout()
+  }
+  self.isAuthed = function() {
+    return auth.isAuthed ? auth.isAuthed() : false
+  }
+}
+
+angular.module('app', [])
+.factory('authInterceptor', authInterceptor)
+.service('user', userService)
+.service('auth', authService)
+.constant('API', 'http://test-routes.herokuapp.com')
+.config(function($httpProvider) {
+  $httpProvider.interceptors.push('authInterceptor');
+})
+.controller('Main', MainCtrl)
+})();
